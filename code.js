6808391f3110cf28d030f6bfe691303ce7946f66
Nathanael Connell <nathanael@Nathanaels-MacBook-Pro.local>
2024-10-22 10:10:48 -0600

    test commit

--- code.js
+++ code.js
@@ -1,8 +1,51 @@
 function flip(array, n) {
+    
+    let tempArray = new array;
+    for (let i = 0; i < n; i++){
+        tempArray.push(array[i]);
+    }
+    tempArray.reverse();
+    array.unshift(tempArray);
+
     return array;
 }
 
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
+
+    let numSort = 0;
+    while (numSort != array.length){
+        let largets = array[0];
+        for (let i = 0; i < (array.length - numsSort); i++){
+            if (array[i] > largest){
+                largest = array[i];
+            }
+        }
+        if ( largest != array[0]){
+            let n = 0;
+            while (n <= array.indexOf(largest)){
+                n++;
+            }
+            flip(array, n);
+            if (numSort == 0){
+                flip (array, array.length + 1);
+                numSort++;
+            }
+            else{
+                flip(array, array.length - numSort);
+                numSort++;
+            }
+        }
+        else{
+            if (numSort == 0){
+                flip(array, array.length + 1);
+                numSort++;
+            }
+            else{
+                flip(array, array.length - numSort);
+                numSort++;
+            }
+        }
+    }
     return array;
 }
